e6a14c8cb55ea100e1a5112f27c214a81fe1ec59
yin_bo_ <3557545112@qq.com>
2025-12-05 13:32:15 +0800

    添加最基本的项目架构

---
+++ gateway/src/main/java/com/yin_bo_/shortlink/gateway/ShortLinkGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.yin_bo_.shortlink.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkGatewayApplication.class, args);
+    }
+}
---
+++ project/src/main/java/com/yin_bo_/shortlink/project/ShortLinkProjectApplication.java
@@ -0,0 +1,11 @@
+package com.yin_bo_.shortlink.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortLinkProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkProjectApplication.class, args);
+    }
+}
