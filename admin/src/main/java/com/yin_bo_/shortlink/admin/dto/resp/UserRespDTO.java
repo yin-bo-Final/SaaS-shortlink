c0b076677864dc65563c237542b1f66e6604d492
yin_bo_ <3557545112@qq.com>
2025-12-06 13:36:59 +0800

    添加用户敏感信息脱敏功能

--- admin/src/main/java/com/yin_bo_/shortlink/admin/dto/resp/UserRespDTO.java
+++ admin/src/main/java/com/yin_bo_/shortlink/admin/dto/resp/UserRespDTO.java
@@ -1,5 +1,7 @@
 package com.yin_bo_.shortlink.admin.dto.resp;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.yin_bo_.shortlink.admin.common.serialize.PhoneDesensitizationSerializer;
 import lombok.Data;
 
 /**
@@ -27,6 +29,7 @@ public class UserRespDTO {
     /**
      * 手机号
      */
+    @JsonSerialize(using = PhoneDesensitizationSerializer.class)
     private String phone;
 
     /**
