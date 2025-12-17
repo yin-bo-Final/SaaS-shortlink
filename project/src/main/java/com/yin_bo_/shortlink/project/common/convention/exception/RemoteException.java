dd241a4d72e1ebfe6e003d9e8b26739407c19f38
yin_bo_ <3557545112@qq.com>
2025-12-17 14:24:19 +0800

    为project模块设置状态码模块

---
+++ project/src/main/java/com/yin_bo_/shortlink/project/common/convention/exception/RemoteException.java
@@ -0,0 +1,31 @@
+package com.yin_bo_.shortlink.project.common.convention.exception;
+
+
+import com.yin_bo_.shortlink.project.common.convention.errorcode.BaseErrorCode;
+import com.yin_bo_.shortlink.project.common.convention.errorcode.IErrorCode;
+
+/**
+ * 远程服务调用异常
+ */
+public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
+
+    public RemoteException(String message, IErrorCode errorCode) {
+        this(message, null, errorCode);
+    }
+
+    public RemoteException(String message, Throwable throwable, IErrorCode errorCode) {
+        super(message, throwable, errorCode);
+    }
+
+    @Override
+    public String toString() {
+        return "RemoteException{" +
+                "code='" + errorCode + "'," +
+                "message='" + errorMessage + "'" +
+                '}';
+    }
+}
