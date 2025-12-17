2928b2b1740c60eccdff26e51744a4646e6262fc
yin_bo_ <3557545112@qq.com>
2025-12-17 14:26:17 +0800

    启动类添加扫描映射

--- project/src/main/java/com/yin_bo_/shortlink/project/ShortLinkProjectApplication.java
+++ project/src/main/java/com/yin_bo_/shortlink/project/ShortLinkProjectApplication.java
@@ -1,8 +1,10 @@
 package com.yin_bo_.shortlink.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.yin_bo_.shortlink.project.dao.mapper")
 @SpringBootApplication
 public class ShortLinkProjectApplication {
     public static void main(String[] args) {
