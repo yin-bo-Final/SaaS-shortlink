be3188b0a9c0280b35aa8916dd40c0f9af3dc91c
yin_bo_ <3557545112@qq.com>
2025-12-17 14:25:47 +0800

    添加短链接模块的实体,映射以及MP的自动填充功能

---
+++ project/src/main/java/com/yin_bo_/shortlink/project/dao/entity/ShortLinkDO.java
@@ -0,0 +1,99 @@
+package com.yin_bo_.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+
+/**
+ * 短链接实体
+ */
+@Data
+@TableName("t_link")
+public class ShortLinkDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+
+    /**
+     * 域名
+     */
+    private String domain;
+
+    /**
+     * 短链接
+     */
+    private String shortUri;
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 原始链接
+     */
+    private String originUrl;
+
+    /**
+     * 点击量
+     */
+    private Integer clickNum;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 启用标识 0：已启用 1：未启用
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer enableStatus;
+
+    /**
+     * 创建类型 0：接口 1：控制台
+     */
+    private Integer createdType;
+
+    /**
+     * 有效期类型 0：永久有效 1：用户自定义
+     */
+    private Integer validDateType;
+
+    /**
+     * 有效期
+     */
+    private LocalDateTime validDate;
+
+    /**
+     * 描述
+     */
+    //这里describe是mysql的关键字，所以我们要转义一下
+    @TableField(value = "`describe`")
+    private String describe;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
---
+++ project/src/main/java/com/yin_bo_/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -0,0 +1,11 @@
+package com.yin_bo_.shortlink.project.dao.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yin_bo_.shortlink.project.dao.entity.ShortLinkDO;
+
+/**
+ * 短链接持久层
+ */
+public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+}
