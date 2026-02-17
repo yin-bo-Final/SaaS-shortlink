93c040adb943bd62e59152acd4a5e63689fd6338
yin-bo-Final <yinbo1054@gmail.com>
2026-02-18 03:17:39 +0800

    feat: 添加MP分页插件

---
+++ project/src/main/java/com/yin_bo_/shortlink/project/config/MybatisPlusConfig.java
@@ -0,0 +1,21 @@
+package com.yin_bo_.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * MP 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
