f40dd18a8c81d2cb1f9e64170a91b4d2beab4b74
yin-bo-Final <yinbo1054@gmail.com>
2026-02-28 03:10:30 +0800

    feat: 添加注释

--- admin/src/main/java/com/yin_bo_/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/yin_bo_/shortlink/admin/controller/GroupController.java
@@ -32,12 +32,22 @@ public class GroupController {
     }
 
 
+    /**
+     * 查看用户所有短链接组
+     * @return 用户所有组
+     */
     @GetMapping("/group")
     public Result<List<GroupRespDTO>> listGroup() {
         return Results.success(groupService.listGroup());
     }
 
 
+    /**
+     * 修改短链接组
+     * @param groupName 组名
+     * @param gid 组id
+     * @return 修改成功
+     */
     @PostMapping("/group/update")
     public Result<Void> updateGroup(@RequestParam String groupName,@RequestParam String gid) {
         groupService.updateGroup(groupName,gid);
@@ -45,6 +55,11 @@ public class GroupController {
     }
 
 
+    /**
+     * 删除短链接组
+     * @param gid 组id
+     * @return 删除成功
+     */
     @PostMapping("/group/remove")
     public Result<Void> removeGroup(@RequestParam String gid) {
         groupService.removeGroup(gid);
@@ -52,6 +67,11 @@ public class GroupController {
     }
 
 
+    /**
+     * 排序短链接组
+     * @param requestParam 请求参数
+     * @return 排序成功
+     */
     @PostMapping("/group/sort")
     public Result<Void> sortGroup(@RequestBody List<GroupSortReqDTO> requestParam) {
         groupService.sortGroup(requestParam);
