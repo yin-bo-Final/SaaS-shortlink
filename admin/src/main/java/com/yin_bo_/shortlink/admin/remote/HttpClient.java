79d32ca319508a124ccbfeb6ca8c2051879af06d
yin-bo-Final <yinbo1054@gmail.com>
2026-02-28 03:14:55 +0800

    fix: 修改http客户端，现在会传输用户信息

--- admin/src/main/java/com/yin_bo_/shortlink/admin/remote/HttpClient.java
+++ admin/src/main/java/com/yin_bo_/shortlink/admin/remote/HttpClient.java
@@ -6,7 +6,11 @@ import cn.hutool.json.JSONUtil;
 import com.yin_bo_.shortlink.admin.common.convention.Result;
 import com.yin_bo_.shortlink.admin.common.convention.errorcode.BaseErrorCode;
 import com.yin_bo_.shortlink.admin.common.convention.exception.RemoteException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 /**
  * 远程 HTTP 客户端
@@ -16,12 +20,25 @@ public class HttpClient {
 
     public <T> Result<T> post(String url, Object body, TypeReference<Result<T>> type) {
         try {
-            String response = HttpRequest.post(url)
+            HttpRequest request = HttpRequest.post(url)
                     .header("Content-Type", "application/json")
                     .body(JSONUtil.toJsonStr(body))
-                    .timeout(5000)
-                    .execute()
-                    .body();
+                    .timeout(5000);
+
+            RequestAttributes attrs = RequestContextHolder.getRequestAttributes();
+            if (attrs instanceof ServletRequestAttributes servletAttrs) {
+                HttpServletRequest servletRequest = servletAttrs.getRequest();
+                String username = servletRequest.getHeader("username");
+                String token = servletRequest.getHeader("token");
+                if (username != null) {
+                    request.header("username", username);
+                }
+                if (token != null) {
+                    request.header("token", token);
+                }
+            }
+
+            String response = request.execute().body();
             return JSONUtil.toBean(response, type, true);
         } catch (Exception ex) {
             throw new RemoteException("调用中台接口失败: " + url, ex, BaseErrorCode.REMOTE_ERROR);
