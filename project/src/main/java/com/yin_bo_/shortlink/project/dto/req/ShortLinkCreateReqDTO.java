c40356561ac670d08684428b4ef0da2e0874546b
yin-bo-Final <yinbo1054@gmail.com>
2026-03-07 20:38:28 +0800

    fix: 短链接创建请求DTO的过期时间上加上时间序列

--- project/src/main/java/com/yin_bo_/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
+++ project/src/main/java/com/yin_bo_/shortlink/project/dto/req/ShortLinkCreateReqDTO.java
@@ -1,5 +1,6 @@
 package com.yin_bo_.shortlink.project.dto.req;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -45,6 +46,7 @@ public class ShortLinkCreateReqDTO {
     /**
      * 有效期
      */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime validDate;
 
 
